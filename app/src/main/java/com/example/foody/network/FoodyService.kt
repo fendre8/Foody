6c1a57c84845c0a6c7eac339958d6f665be3cbdc
Endre Ádám Forgács <fendre8@gmail.com>
2023-05-07 22:39:09 +0200

    Implement FoodyService and network layer

--- app/src/main/java/com/example/foody/network/FoodyService.kt
+++ app/src/main/java/com/example/foody/network/FoodyService.kt
@@ -1,4 +1,23 @@
 package com.example.foody.network
 
-class FoodyService {
+import CategoryArray
+import MealArray
+import MealMinimalArray
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface FoodyService {
+
+    @GET("search.php")
+    fun fetchMealList(@Query("s") s: String): Call<MealArray>
+
+    @GET("lookup.php")
+    fun lookUpMeal(@Query("i") i: String): Call<MealArray>
+
+    @GET("categories.php")
+    fun listAllCategories(): Call<CategoryArray>
+
+    @GET("filter.php")
+    fun filterCategories(@Query("c") c: String): Call<MealMinimalArray>
 }
---
+++ app/src/main/java/com/example/foody/network/NetworkConfiguration.kt
@@ -0,0 +1,5 @@
+package com.example.foody.network
+
+object NetworkConfiguration {
+    const val ENDPOINT_ADDRESS = "https://themealdb.com/api/json/v1/1/"
+}
