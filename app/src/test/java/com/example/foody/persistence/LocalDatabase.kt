3f06f7a5e4dd0b62ad15950c1aad9d9869f3da56
Endre Ádám Forgács <fendre8@gmail.com>
2023-05-07 23:42:17 +0200

    Add db tests base

---
+++ app/src/test/java/com/example/foody/persistence/LocalDatabase.kt
@@ -0,0 +1,27 @@
+package com.example.foody.persistence
+
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import org.junit.After
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [33])
+abstract class LocalDatabase {
+    lateinit var db: AppDatabase
+
+    @Before
+    fun initDB() {
+        db = Room.inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext(), AppDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+    }
+
+    @After
+    fun closeDB() {
+        db.close()
+    }
+}
